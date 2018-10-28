77b0415c2acd5bc5f265db1cfe59006b7c77869a
DonMins <maks000156@yandex.ru>
2018-10-28 12:58:38 +0400

    Update mainRun.py

--- mainRun.py
+++ mainRun.py
@@ -2,7 +2,7 @@ import scipy.special as spc
 import numpy as np
 import scipy as sp
 import plotly.offline as py
-
+import matplotlib.pyplot as plt
 
 
 parameter_array = {
@@ -28,7 +28,7 @@ def updateParameret_array(dan):
 
 # надо как нибудь оптимизировать
 bessel0 = [0, ]
-temp = spc.jn_zeros(1, 500)
+temp = spc.jn_zeros(1, 100000)
 
 for t in temp:
     bessel0.append(t)
@@ -51,7 +51,41 @@ def An(n,t):
     all = numerator/denominator
     return all * (1- np.exp(x))
 
-def u(r,t, esp, date=None):
+def analitic_eps(rank):
+    rank = 10**(-rank)
+    N = ((2*parameter_array['betta']*parameter_array['P']*10**(1/2))/ (15*(parameter_array['a'])**2 *parameter_array['k']*parameter_array['l']*
+                                                                      (np.pi)**3 * rank))**(2/3)
+    return round(N)
+
+flag=[]
+NMAX=[0]
+
+
+def experimental_eps(r, t, eps, date=None):
+    if date != None:
+        updateParameret_array(date)
+
+    numerator = (parameter_array['l'] * parameter_array['betta'] * parameter_array['P'])
+    denominator = (50 * sp.pi * parameter_array['alf'] * parameter_array['a'] ** 2)
+    all = numerator / denominator
+    x = ((-2 * parameter_array['alf'] * t) / (parameter_array['c'] * parameter_array['l']))
+    u = all * (1 - np.exp(x))
+
+    N = analitic_eps(eps)
+
+    for i in np.arange(1, N+1, 1):
+        u += An(i, t) * spc.j0((bessel0[i] * r) / parameter_array['R'])
+        flag.append(u)
+    i=N-2
+    analit_eps = flag[N-1]
+
+    while (abs ( analit_eps - flag[i])<=10**(-eps)):
+        i=i-1
+    if ((i+2)>NMAX[0]):
+        NMAX[0] = i+2
+    return u
+
+def u(r,t, eps,colslag,date=None):
     if date != None:
         updateParameret_array(date)
 
@@ -60,23 +94,28 @@ def u(r,t, esp, date=None):
     all = numerator/denominator
     x = ((-2*parameter_array['alf']*t)/(parameter_array['c']*parameter_array['l']))
     u = all * (1- np.exp(x))
+    if (colslag==0):
+        N = analitic_eps(eps)
+        for i in np.arange(1,N,1):
+            u +=An(i, t) * spc.j0((bessel0[i] * r) / parameter_array['R'])
+    else:
+        for i in np.arange(1,colslag,1):
+            u +=An(i, t) * spc.j0((bessel0[i] * r) / parameter_array['R'])
 
-    for i in np.arange(1,200,1):
-        u +=An(i, t) * spc.j0((bessel0[i] * r) / parameter_array['R'])
     return u
 
 def getparams():
     return parameter_array
 
 
-def drawUIT(da, eps):
+def drawUIT(da, eps,colSlag):
     data = [dict(
     visible=False,
     line=dict(color='#ff0000', width=3),
     name='t = ' + str(step),
-    x=np.arange(0, 5, 0.1),
-    y=[u(t, step, eps, da) for t in np.arange(0, 5, 0.1)]) for step in
-    range(180)]
+    x=np.arange(0, 4.1, 0.1),
+    y=[u(t, step, eps,colSlag, da) for t in np.arange(0, 4.1, 0.1)]) for step in
+    range(1,181)]
     steps = []
     for i in range(len(data)):
         step = dict(
@@ -94,13 +133,13 @@ def drawUIT(da, eps):
     fig = dict(data=data, layout=layout)
     py.plot(fig, filename='Temperature in time.html')
 
-def drawUIR(da, eps):
+def drawUIR(da, eps,colSlag):
     data = [dict(
     visible=False,
     line=dict(color='#ff0000', width=3),
     name='r = ' + str(step),
-    x=np.arange(100),
-    y=[u(step, t, eps, da) for t in np.arange(100)]) for step in np.arange(0, 5, 0.1)]
+    x=np.arange(181),
+    y=[u(step, t, eps,colSlag,da) for t in np.arange(1,181)]) for step in np.arange(0, 4.1, 0.1)]
 
     steps = []
 
@@ -120,35 +159,98 @@ def drawUIR(da, eps):
     fig = dict(data=data, layout=layout)
     py.plot(fig, filename='Temperature in radius.html')
 
+def plot_eps(x,y):
 
-da = getparams()
-print("Текущие параметры")
-print("R = {0} l = {1} k = {2}".format(da['R'], da['l'], da['k']))
-print("alfa = {0} c = {1} betta = {2}".format(da['alf'], da['c'],da['betta']))
-print("P = {0}, a = {1}, T={2}".format(da['P'], da['a'],da['T']))
-yes = input("Введите Yes, если хотите изменить параметры ")
-if yes.upper() == "YES":
-    R = input("Введите R ")
-    l = input("Введите l ")
-    k = input("Введите k ")
-    alfa = input("Введите alfa ")
-    c = input("Введите c ")
-    betta = input("Введите betta ")
-    P = input("Введите P ")
-    T = input("Введите T ")
-    da['R'] = float(R)
-    da['l'] = float(l)
-    da['k'] = float(k)
-    da['alf'] = float(alfa)
-    da['c'] = float(c)
-    da['betta'] = float(betta)
-    da['P'] = float(P)
-    da['a'] = float(R) / 5
-    da['T'] = float(T)
+    fig=  plt.figure("График погрешности ")
+    ax = fig.add_subplot(111)
+    # ax.set_xticks([10e-7,10e-6,10e-5,10e-4,10e-3,10e-2,10e-1])
+    plt.grid(True)
+    plt.plot(x, y, marker='o',markersize = 3,markeredgecolor = '#ff0000')
+    plt.plot(x, y,linewidth = 1,color = '#ff0000')
+    plt.show()
 
-#eps = int(input("Введите точность (целоечисло -количество знаков после запятой)"))
-eps=1
-print("Подождите, пожалуйста, выполняются вычисления...")
-drawUIR(da, eps)
-drawUIT(da, eps)
-print(spc.j0(0))
+exit = 0
+while(exit==0):
+    da = getparams()
+    print("Текущие параметры")
+    print("R = {0} l = {1} k = {2}".format(da['R'], da['l'], da['k']))
+    print("alfa = {0} c = {1} betta = {2}".format(da['alf'], da['c'], da['betta']))
+    print("P = {0}, a = {1}, T={2}".format(da['P'], da['a'], da['T']))
+    yes = input("Введите +, если хотите изменить параметры  иначе - ")
+
+    if yes.upper() == "+":
+        R = input("Введите R ")
+        l = input("Введите l ")
+        k = input("Введите k ")
+        alfa = input("Введите alfa ")
+        c = input("Введите c ")
+        betta = input("Введите betta ")
+        P = input("Введите P ")
+        T = input("Введите T ")
+        a = input("Введите a ")
+        da['R'] = float(R)
+        da['l'] = float(l)
+        da['k'] = float(k)
+        da['alf'] = float(alfa)
+        da['c'] = float(c)
+        da['betta'] = float(betta)
+        da['P'] = float(P)
+        da['a'] = float(a)
+        da['T'] = float(T)
+        updateParameret_array(da)
+        da = getparams()
+        print("Текущие параметры")
+        print("R = {0} l = {1} k = {2}".format(da['R'], da['l'], da['k']))
+        print("alfa = {0} c = {1} betta = {2}".format(da['alf'], da['c'], da['betta']))
+        print("P = {0}, a = {1}, T={2}".format(da['P'], da['a'], da['T']))
+
+    scoreOrSeries = (input("Хотите вычислить ряд ( введите - 1) или погрешность (введите 2) ? "))
+
+    if scoreOrSeries == "1":
+        yes_no = (input(
+            "Хотите посчитать ряд используя точность( введите - 1) или количество слагаемых для посчета(введите 2) ? "))
+
+        if yes_no == "1":
+            eps = int(input("Введите точность (целоечисло -количество знаков после запятой)"))
+            print("Подождите, пожалуйста, выполняются вычисления...")
+            drawUIR(da, eps, 0)
+            drawUIT(da, eps, 0)
+
+        if yes_no == "2":
+            n = int(input("Введите количество слагаемых N = "))
+            print("Подождите, пожалуйста, выполняются вычисления...")
+            drawUIR(da, eps, n)
+            drawUIT(da, eps, n)
+
+    if scoreOrSeries == "2":
+        error = (input(
+            "Рассчитать кол -во слагаемых для достижени конкретной точности (введите 1 ) или для всего ряда (введите 2 (это займет много времени ))"))
+        if error == "1":
+            ep = int(input("Введите точность (целоечисло -количество знаков после запятой)"))
+            print("Подождите, пожалуйста, выполняются вычисления...")
+            for t in np.arange(1, da['T'] + 1):
+                for r in np.arange(0, da['R'] + 0.1, 0.1):
+                    experimental_eps(r, t, ep, da)
+            print("NMAX избыточое для eps = 10^-", ep, "равно ", analitic_eps(ep))
+            print("NMAX достаточное для eps = 10^-", ep, "равно ", NMAX)
+            NMAX[0] = 0
+            flag.clear()
+
+        if error == "2":
+            print("Подождите, пожалуйста, выполняются вычисления...")
+            Y = []
+            EPS = [1, 2, 3, 4, 5, 6, 7]  # степени
+            for ep in EPS:
+                for t in np.arange(1, da['T'] + 1):
+                    for r in np.arange(0, da['R'] + 0.1, 0.1):
+                        experimental_eps(r, t, ep, da)
+                print("NMAX избыточое для eps = 10^-", ep, "равно ", analitic_eps(ep))
+                print("NMAX достаточное для eps = 10^-", ep, "равно ", NMAX)
+                Y.append(NMAX[0])
+                NMAX[0] = 0
+                flag.clear()
+            X = [16, 32, 64, 128, 256, 512, 1024]  # для удобства построения погрешности 16 - 10e-7, 32 - 10e-6 и т.д
+            Y.reverse()
+            plot_eps(X, Y)
+    exit = int(input("Для выхода нажмите - 1 , чтобы продолжить - 0"))
+    print("-------------------------------------------------------------------------------------")
